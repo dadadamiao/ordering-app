f2ddf1898bb2b691cd6d78bface779b84a057ccc
dadadamiao <88427911+dadadamiao@users.noreply.github.com>
2026-03-11 17:33:35 +0800

    Initial commit: Added App.js for the React Ordering App

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Ordering App</h1>
+      <Menu />
+      <Cart />
+    </div>
+  );
+};
+
+export default App;
